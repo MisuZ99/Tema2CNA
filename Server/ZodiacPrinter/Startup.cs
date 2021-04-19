8a78a3b8cacf41ebded3677ed843989eb129049d
MisuZ99 <chromezephyr99@gmail.com>
2021-04-19 14:54:15 +0300

    Created the client and changed the program settings

--- Server/ZodiacPrinter/Startup.cs
+++ Server/ZodiacPrinter/Startup.cs
@@ -31,7 +31,28 @@ namespace ZodiacPrinter
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<PrinterService>();
+                endpoints.MapGrpcService<SummerService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+
+                endpoints.MapGrpcService<SpringService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+
+                endpoints.MapGrpcService<WinterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+
+                endpoints.MapGrpcService<AutumnService>();
 
                 endpoints.MapGet("/", async context =>
                 {
