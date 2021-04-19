307d36f9eded3c69166a7a22159bf4fdec048e44
MisuZ99 <chromezephyr99@gmail.com>
2021-04-19 17:17:40 +0300

    Created a response for valid inputs

---
+++ Client/ZodiacPrinterClient/CalendarDate.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZodiacPrinterClient
+{
+    class CalendarDate
+    {
+        private int month;
+        public int Month
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                month = value;
+            }
+        }
+
+        private int day;
+        public int Day
+        {
+            get
+            {
+                return day;
+            }
+            set
+            {
+                day = value;
+            }
+        }
+
+        private int year;
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+
+        public static CalendarDate ConvertToCalendarDate(string date)
+        {
+            CalendarDate newDate = new CalendarDate();
+            int month = 0;
+            int day = 0;
+            int year = 0;
+            int i = 0;
+            while (!date[i].Equals('/'))
+            {
+                month = month * 10 + (int)Char.GetNumericValue(date[i]);
+                i++;
+            }
+            i++;
+            while (!date[i].Equals('/'))
+            {
+
+                day = day * 10 + (int)Char.GetNumericValue(date[i]);
+                i++;
+            }
+            while (i < date.Length)
+            {
+                year = year * 10 + (int)Char.GetNumericValue(date[i]);
+                i++;
+            }
+
+            newDate.Year = year;
+            newDate.Month = month;
+            newDate.Day = day;
+            return newDate;
+        }
+    }
+}
--- Client/ZodiacPrinterClient/Program.cs
+++ Client/ZodiacPrinterClient/Program.cs
@@ -9,18 +9,43 @@ namespace ZodiacPrinterClient
        
         static async Task Main(string[] args)
         {
-            // The port number(5001) must match the port of the gRPC server.
-            /* using var channel = GrpcChannel.ForAddress("https://localhost:7580");
-             var client = new Spring.SpringClient(channel);
-             var reply = client.SpringSign(
-                               new SpringRequest {  });
-             Console.WriteLine( reply.ZodiacSign);*/
+            Console.WriteLine("date: ");
+            string date = Console.ReadLine();
 
-            using var channel = GrpcChannel.ForAddress("https://localhost:7580");
-            var client = new Autumn.AutumnClient(channel);
-            var reply = client.AutumnSign(
-                              new AutumnRequest {Date="04/09/2010" });
-            Console.WriteLine(reply.ZodiacSign);
+            CalendarDate newDate = CalendarDate.ConvertToCalendarDate(date);
+
+            if (newDate.Month == 1 || newDate.Month == 2 || newDate.Month == 12)
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:7580");
+                var client = new Winter.WinterClient(channel);
+                var reply = client.WinterSign(
+                                  new WinterRequest { Date = date });
+                Console.WriteLine(reply.ZodiacSign);
+            }
+            else if (newDate.Month == 3 || newDate.Month == 4 || newDate.Month == 5)
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:7580");
+                var client = new Spring.SpringClient(channel);
+                var reply = client.SpringSign(
+                                  new SpringRequest {Date=date });
+                Console.WriteLine(reply.ZodiacSign);
+            }
+            else if (newDate.Month == 6 || newDate.Month == 7 || newDate.Month == 8)
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:7580");
+                var client = new Summer.SummerClient(channel);
+                var reply = client.SummerSign(
+                                  new SummerRequest {Date=date });
+                Console.WriteLine(reply.ZodiacSign);
+            }
+            else
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:7580");
+                var client = new Autumn.AutumnClient(channel);
+                var reply = client.AutumnSign(
+                                  new AutumnRequest { Date = date });
+                Console.WriteLine(reply.ZodiacSign);
+            }
 
         }
     }
