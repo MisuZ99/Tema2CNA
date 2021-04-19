c431add31773df98a6f725aec5e871be4c2c8aff
MisuZ99 <chromezephyr99@gmail.com>
2021-04-19 14:53:45 +0300

    Created services for all seasons

---
+++ Server/ZodiacPrinter/Services/SummerService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ZodiacPrinter
+{
+    public class SummerService : Summer.SummerBase
+    {
+
+        public override Task<SummerReply> SummerSign(SummerRequest request, ServerCallContext context)
+        {
+
+            if (request.Date != null)
+            {
+
+                Console.WriteLine("Client conected on Summer!");
+            }
+            else
+            {
+                Console.WriteLine(request.Date + " wrong input!");
+            }
+
+            return Task.FromResult(new SummerReply
+            {
+                ZodiacSign = "Capricorn"
+            });
+        }
+    }
+}
---
+++ Server/ZodiacPrinter/Services/WinterService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ZodiacPrinter
+{
+    public class WinterService : Winter.WinterBase
+    {
+
+        public override Task<WinterReply> WinterSign(WinterRequest request, ServerCallContext context)
+        {
+
+            if (request.Date != null)
+            {
+
+                Console.WriteLine("Client connected on Winter!");
+            }
+            else
+            {
+                Console.WriteLine(request.Date + " wrong input!");
+            }
+
+            return Task.FromResult(new WinterReply
+            {
+                ZodiacSign = "Pesti"
+            });
+        }
+    }
+}
+
